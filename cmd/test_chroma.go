cfa4eed644f38c19f6d4663818709636ddae5ce4
Patrick Vogt <patrick.vogt@unibas.ch>
2024-10-17 14:48:14 +0200

    working tests

--- cmd/test_chroma.go
+++ cmd/test_chroma.go
@@ -1,10 +1,12 @@
 package cmd
 
 import (
-	"context"
 	"fmt"
 	"log/slog"
+	"strings"
 
+	"github.com/amikos-tech/chroma-go/types"
+	"github.com/google/uuid"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"github.com/tmc/langchaingo/chains"
@@ -17,11 +19,19 @@ import (
 
 func addchroma() {
 	testCmd.AddCommand(chromaCmd)
+	chromaCmd.AddCommand(chromaDelCmd)
 }
+
+const (
+	deleteCollectionFlag = "delete"
+	chromeURL            = "http://localhost:8000"
+	index                = "vogtp_test_rag"
+)
 
 func chromaFlags() {
 	addFlagOllamaUrl(chromaCmd)
-	if err := viper.BindPFlags(chromaCmd.Flags()); err != nil {
+	chromaCmd.PersistentFlags().Bool(deleteCollectionFlag, false, "delete collection")
+	if err := viper.BindPFlags(chromaCmd.PersistentFlags()); err != nil {
 		slog.Warn("cannot bind chroma flags", "err", err)
 	}
 }
@@ -34,23 +44,56 @@ var chromaCmd = &cobra.Command{
 		DisableDefaultCmd: true,
 	},
 	RunE: func(cmd *cobra.Command, args []string) error {
-		return chromaVecDB(cmd.Context())
+		return chromaVecDB(cmd)
 	},
 }
 
-func chromaVecDB(ctx context.Context) error {
-	chromeURL := "http://localhost:8000"
-	index := "vogtp_test_rag"
+var chromaDelCmd = &cobra.Command{
+	Use:     "delete",
+	Short:   "delete collection",
+	Aliases: []string{"del", "d"},
+	CompletionOptions: cobra.CompletionOptions{
+		DisableDefaultCmd: true,
+	},
+	RunE: func(cmd *cobra.Command, args []string) error {
 
-	llm, e, err := getEmbedding("gemma")
+		_, e, err := getEmbedding("mxbai-embed-large")
+		if err != nil {
+			return err
+		}
+		store, err := chroma.New(
+			chroma.WithChromaURL(chromeURL),
+			chroma.WithNameSpace(index),
+			chroma.WithEmbedder(e),
+		)
+		if err != nil {
+			return fmt.Errorf("cannot create chroma client: %w", err)
+		}
+		if err := store.RemoveCollection(); err != nil {
+			return fmt.Errorf("cannot remove collection: %w", err)
+		}
+		return nil
+	},
+}
+
+func chromaVecDB(cmd *cobra.Command) error {
+	ctx := cmd.Context()
+
+	llm, err := getOllamaClient("llama3.1")
+	if err != nil {
+		return fmt.Errorf("cannot load llama3.1: %w", err)
+	}
+
+	_, e, err := getEmbedding("mxbai-embed-large")
 	if err != nil {
 		return err
 	}
-
+	index := uuid.New().String()
 	store, err := chroma.New(
 		chroma.WithChromaURL(chromeURL),
 		chroma.WithNameSpace(index),
 		chroma.WithEmbedder(e),
+		chroma.WithDistanceFunction(types.COSINE),
 	)
 	if err != nil {
 		return fmt.Errorf("cannot create chroma client: %w", err)
@@ -65,38 +108,61 @@ func chromaVecDB(ctx context.Context) error {
 		{PageContent: "Toyota", Metadata: map[string]any{"population": 0.42, "area": 918}},
 		{PageContent: "Fukuoka", Metadata: map[string]any{"population": 1.59, "area": 341}},
 		{PageContent: "Paris", Metadata: map[string]any{"population": 11, "area": 105}},
-		{PageContent: "London", Metadata: map[string]any{"population": 9.5, "area": 1572}},
+		{PageContent: "London", Metadata: map[string]any{"population": 19.5, "area": 1572}},
 		{PageContent: "Santiago", Metadata: map[string]any{"population": 6.9, "area": 641}},
 		{PageContent: "Buenos Aires", Metadata: map[string]any{"population": 15.5, "area": 203}},
 		{PageContent: "Rio de Janeiro", Metadata: map[string]any{"population": 13.7, "area": 1200}},
 		{PageContent: "Sao Paulo", Metadata: map[string]any{"population": 22.6, "area": 1523}},
 	}
+	for i, d := range data {
+		meta := make([]string, 0)
+		for k, v := range d.Metadata {
+			meta = append(meta, fmt.Sprintf("%s=%v", k, v))
+		}
+		d.PageContent = fmt.Sprintf("%s \nMetadata: %s", d.PageContent, strings.Join(meta, ", "))
+		data[i] = d
+	}
+	for _, d := range data {
+		fmt.Printf("%+v\n", d)
+	}
 
 	ids, err := store.AddDocuments(ctx, data)
 	if err != nil {
-		return fmt.Errorf("cannot add docs: %w", err)
+		slog.Warn("Cannot add docs", "cnt", len(data), "err", err)
+		//return fmt.Errorf("cannot add docs: %w", err)
 	}
-	slog.Info("Added docs to vecDB", "cnt", len(ids))
-	docs, err := store.SimilaritySearch(ctx, "yo", 2,
-		vectorstores.WithScoreThreshold(0.5),
-	)
-	if err != nil {
-		return fmt.Errorf("cannot search the docs: %w", err)
-	}
-	fmt.Printf("found docs: %v\n", docs)
+	slog.Info("Added docs to vecDB", "cnt", len(ids), "ids", strings.Join(ids, ","))
 
-	result, err := chains.Run(
-		ctx,
-		chains.NewRetrievalQAFromLLM(
-			llm,
-			vectorstores.ToRetriever(store, 5, vectorstores.WithScoreThreshold(0.8)),
-		),
-		"City with a population of more than 5",
-	)
-	if err != nil {
-		return fmt.Errorf("cannot run chain: %w", err)
+	for _, question := range []string{"london population", "london", "City with a population of more than 15"} {
+		docs, err := store.SimilaritySearch(ctx, question, 3, vectorstores.WithScoreThreshold(0.3))
+
+		if err != nil {
+			return fmt.Errorf("cannot search the docs: %w", err)
+		}
+		fmt.Printf("**************\nQuestion: %s\nDocs: %v\n", question, len(docs))
+		for i, d := range docs {
+			fmt.Printf("Doc %v score: %v -> %v %v\n", i, d.Score, d.PageContent, d.Metadata)
+		}
+
+		result, err := chains.Run(
+			ctx,
+			chains.NewRetrievalQAFromLLM(
+				llm,
+				vectorstores.ToRetriever(
+					store,
+					5,
+					//vectorstores.WithNameSpace(index),
+					//vectorstores.WithScoreThreshold(0.8),
+				),
+			),
+			question,
+			//"City with a population of more than 5",
+		)
+		if err != nil {
+			return fmt.Errorf("cannot run chain: %w", err)
+		}
+		fmt.Printf("Res: %v\n", result)
 	}
-	fmt.Println(result)
 	return nil
 }
 
