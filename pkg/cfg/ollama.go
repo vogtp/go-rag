860db55caa99dbb3d07d8f26a151b674aab48826
Patrick Vogt <patrick.vogt@unibas.ch>
2025-01-30 13:11:15 +0100

    make check for running ollama robust

--- pkg/cfg/ollama.go
+++ pkg/cfg/ollama.go
@@ -14,21 +14,35 @@ var _ollamaHost string
 
 // GetOllamaHost returns a active ollama host
 func GetOllamaHost(ctx context.Context) string {
-	if _ollamaHost != "" {
+	if checkOllama(ctx, _ollamaHost) {
+		slog.Info("Using prevously found ollala host", "url", _ollamaHost)
 		return _ollamaHost
 	}
 	for _, o := range viper.GetStringSlice(ollamaHosts) {
-		u, err := url.Parse(o)
-		if err != nil {
-			slog.Warn("Cannot parse ollama url", "url", o, "err", err)
-			continue
+		if checkOllama(ctx, o) {
+			_ollamaHost = o
+			slog.Info("Found running ollama", "url", o)
+			return _ollamaHost
 		}
-		c := ollamaAPI.NewClient(u, http.DefaultClient)
-		if err := c.Heartbeat(ctx); err != nil {
-			slog.Warn("Cannot connect to ollama", "url", o, "err", err)
-		}
-		_ollamaHost = o
-		break
 	}
-	return _ollamaHost
+	slog.Warn("No running ollama found")
+	return ""
+}
+
+func checkOllama(ctx context.Context, urlStr string) bool {
+	slog.Debug("Checking ollama host", "url", urlStr)
+	if len(urlStr) < 7 { // 7 -> http://
+		return false
+	}
+	u, err := url.Parse(urlStr)
+	if err != nil {
+		slog.Warn("Cannot parse ollama url", "url", urlStr, "err", err)
+		return false
+	}
+	c := ollamaAPI.NewClient(u, http.DefaultClient)
+	if err := c.Heartbeat(ctx); err != nil {
+		slog.Warn("Cannot connect to ollama", "url", u, "err", err)
+		return false
+	}
+	return true
 }
