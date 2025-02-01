c7a88387929a17bd39df88ed9f7ec5bcf5d016d5
Patrick Vogt <patrick.vogt@unibas.ch>
2025-02-01 13:19:16 +0100

    ng toolbar and footer

--- pkg/web/ng/intrasearch/src/app/app.component.ts
+++ pkg/web/ng/intrasearch/src/app/app.component.ts
@@ -1,7 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   standalone: true,
@@ -9,7 +10,9 @@ import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
     CommonModule,
     RouterOutlet,
     RouterLink,
-    RouterLinkActive
+    RouterLinkActive,  
+    MatToolbarModule,
+    MatIcon,
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
