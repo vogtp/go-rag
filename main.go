45e4da0ef130a2507e0d90315d47f02bcb5879ce
Patrick Vogt <patrick.vogt@unibas.ch>
2025-02-27 08:35:36 +0100

    better signal handling

--- main.go
+++ main.go
@@ -2,28 +2,14 @@ package main
 
 import (
 	"context"
-	"log/slog"
 	"os"
 	"os/signal"
-	"syscall"
 
 	"github.com/spf13/cobra"
 	"github.com/vogtp/rag/cmd"
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt, syscall.SIGHUP, syscall.SIGTERM)
-
-	go func() {
-		select {
-		case s := <- signals:
-			slog.Warn("Got signal", "sig", s)
-			cancel()
-		case <-ctx.Done():
-		}
-	}()
-
+	ctx, _ := signal.NotifyContext(context.Background(), os.Kill, os.Interrupt)
 	cobra.CheckErr(cmd.New().ExecuteContext(ctx))
 }
